61fb4f065c01ade8fe3a0ef3f2811b844c228389
dinushkaherath <dinushka.herath@gmail.com>
2020-05-14 09:17:21 -0400

    converted to verseLookup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="tabs">
+      <div v-if="tabs" class="tab">Select Verses</div>
+      <div v-if="!tabs" @click="reset()">
+        <img src="@/ic_chevron_right_48px.svg" alt />
+      </div>
+      <div v-if="verseBar" class="bar">Selected Verses</div>
+    </div>
+    <form v-if="tabs" id="app" @submit.prevent="submitVerseList">
+      <p>Example: Matt. 1; Matthew 2:4; Matt. 5:18-20; Psa. 145-146; Psalm 140:12-141:9</p>
+      <p>
+        <label for="verseList">Enter Verses:</label>
+        <input v-model="verseList" type="text" name="verseList" />
+      </p>
+      <p>
+        <input type="submit" value="Submit" />
+      </p>
+    </form>
+    <Verses v-if="!tabs" :verseList="verse" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Verses from "@/components/Verses.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Verses
+  },
+  data: () => {
+    return {
+      tabs: true,
+      verseBar: false,
+      verse: "Isaiah 24:21-25:12"
+    };
+  },
+  methods: {
+    submitVerseList() {
+      this.verse = this.verseList;
+      this.tabs = false;
+      this.verseBar = true;
+    },
+    reset() {
+      this.tabs = true;
+      this.verseBar = false;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  font-family: "Lora", "Georgia", "Times New Roman", serif;
+}
+body {
+  margin: 0;
+  position: relative;
+  top: 52px;
+  background-color: rgb(246, 246, 246);
+}
+.tabs {
+  position: fixed;
+  background-color: #0b1b27;
+  display: flex;
+  left: 0;
+  top: 0;
+  z-index: 99;
+  width: 100%;
+}
+
+.tab {
+  color: #26ffd4;
+  font-size: 1.2rem;
+  font-weight: bold;
+  justify-content: center;
+  padding: 15px;
+  width: 100%;
+  display: flex;
+}
+.bar {
+  position: relative;
+  color: #26ffd4;
+  font-size: 1.2rem;
+  font-weight: bold;
+  padding: 15px;
+  justify-content: center;
+  width: 100%;
+  display: flex;
+}
+form {
+  align-items: center;
+}
+</style>
+
+<style scoped>
+img {
+  width: 30px;
+  height: 30px;
+  margin: 10px;
+  transform: translateY(0%) rotate(180deg);
+  filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(10deg) brightness(104%)
+    contrast(102%);
 }
 </style>
---
+++ src/components/Verse.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-if="verse.chapter" class="chapter">{{verse.chapter}}</div>
+    <div class="row">
+      <span class="number">{{verseNumber}}</span>
+      <span class="text">{{verseText}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Verse",
+  props: ["verse"],
+  data() {
+    return {
+      verseNumber: this.verse.ref.split(":")[1],
+      verseText:
+        this.verse.ref == "Rom. 16:24" ||
+        this.verse.ref == "Mark 9:44" ||
+        this.verse.ref == "Mark 9:46"
+          ? this.verse.text.replace(/\\/g, "").replace(/\//g, "")
+          : this.verse.text.replace(/\[/g, "").replace(/\]/g, "")
+    };
+  }
+};
+</script>
+
+<style scoped>
+.row {
+  margin: 17px 10px;
+}
+.chapter {
+  font-size: 20px;
+  margin: 0 10px;
+  margin-top: 17px;
+  font-weight: bold;
+}
+.number {
+  font-size: 12px;
+  font-weight: bold;
+  vertical-align: top;
+  text-align: left;
+  margin-right: 5px;
+}
+/* .text {
+  
+} */
+</style>
